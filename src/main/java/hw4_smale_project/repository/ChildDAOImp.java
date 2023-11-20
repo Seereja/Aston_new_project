4af96a70eb8f4ee1b772f2215b3ec3c80a2024e7
Sergey <shilo1233@gmail.com>
2023-11-20 17:06:04 +0300

    добавил Transaction

--- src/main/java/hw4_smale_project/repository/ChildDAOImp.java
+++ src/main/java/hw4_smale_project/repository/ChildDAOImp.java
@@ -6,6 +6,7 @@ import hw4_smale_project.model.Child;
 import jakarta.transaction.Transactional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.sql.Connection;
@@ -20,28 +21,35 @@ public class ChildDAOImp implements ChildDAO {
     @Override
     @Transactional
     public List<Child> getAllChildren() throws SQLException {
-        Session session =  DBConfig.getSessionFactory().openSession();
-        List<Child> children = session.createQuery("from Child ", Child.class).getResultList();
-        return children;
+        Transaction transaction = null;
+        try (Session session = DBConfig.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            List<Child> children = session.createQuery("from Child ", Child.class).getResultList();
+            transaction.commit();
+            return children;
+        }
     }
-
 
     @Override
     public void saveChildren(Child child) {
-        Session session =  DBConfig.getSessionFactory().openSession();
+        Transaction transaction = null;
+        Session session = DBConfig.getSessionFactory().openSession();
+        transaction = session.beginTransaction();
         session.persist(child);
+        transaction.commit();
+
     }
 
     @Override
     public Child getChildren(int id) {
-        Session session =  DBConfig.getSessionFactory().openSession();
+        Session session = DBConfig.getSessionFactory().openSession();
         Child child = session.get(Child.class, id);
         return child;
     }
 
     @Override
     public void deleteChildren(int id) {
-        Session session =  DBConfig.getSessionFactory().openSession();
+        Session session = DBConfig.getSessionFactory().openSession();
         Query<Child> childQuery = session.createQuery("delete from Child where id=:id");
         childQuery.setParameter("id", id);
         childQuery.executeUpdate();
@@ -51,7 +59,7 @@ public class ChildDAOImp implements ChildDAO {
     @Transactional
     public List<Child> getChildrenForSectionId(int id) throws ClassNotFoundException {
         List<Child> children;
-        Session session =  DBConfig.getSessionFactory().openSession();
+        Session session = DBConfig.getSessionFactory().openSession();
         String hql = "SELECT c.id, c.name, c.surname, c.age, s.name FROM Child c INNER JOIN c.sections s WHERE s.id = :id";
         children = session.createQuery(hql, Child.class).setParameter("id", id).getResultList();
         return children;
